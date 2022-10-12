116c37c710f615d9b342941c45c8e211d18d761e
bridgetisvirtuallyhere <bridgetjacosta@gmail.com>
2022-10-12 13:42:37 -0700

    day1

--- src/ForLoops.java
+++ src/ForLoops.java
@@ -3,7 +3,16 @@ import java.util.Scanner;
 public class ForLoops {
     private final static Scanner s = new Scanner(System.in);
 
-    public static void forSum() {
+    public static int forSum() { //In the ForLoops class, create a method named forSum
+        //that calculates the sum 1+2+3+...+n where n is given as user input:
+        System.out.println("Enter numbers: ");
+    int n = Integer.parseInt(s.nextLine());
+    int sum = 0;
+    for(int i=1; 1<=n; i++){
+        sum = sum + i;
+    }
+        System.out.println(sum);
+        return n;
     }
 
     public static void rangeSum() {
--- src/WhileLoops.java
+++ src/WhileLoops.java
@@ -1,12 +1,54 @@
 import java.util.Scanner;
-
+/*
+* @author Bridget Acosta
+* 10/12/2022
+ */
 public class WhileLoops {
     private final static Scanner s = new Scanner(System.in);
 
-    public static void eoCount() {
+    public static void eoCount() { //In the WhileLoops class, write a method named eoCount
+
+        int even = 0;
+        int odd = 0;
+
+        System.out.println("Type numbers:");
+
+        int numbers;
+        while (true) {
+            numbers = Integer.parseInt(s.nextLine());
+
+            if (numbers % 2 == 1) {
+                odd++; // and counts the even and odd numbers
+            }
+            if (numbers % 2 == 0) {
+                even++;
+            }
+            System.out.println("Even numbers: " + even);
+            System.out.println("Odd numbers: " + odd);
+            System.out.println("Type more numbers:");
+            if (numbers == 0) {
+                break;
+            }
+        }
     }
 
-    public static void mean() {
+    public static double mean() {   // In the WhileLoops class, write a method named mean
+
+        int i=0;
+        float sum=0;
+
+        System.out.println("Enter the size of array: ");
+        int n = s.nextInt(); //that contains a while loop reads in integers from System.in
+        float arr[] = new float[n];
+        System.out.println("Enter " + n + " array elements: ");
+        while(i<n) { //and calculates the mean of the numbers entered.
+            arr[i] = s.nextInt();
+            sum=sum+arr[i];
+            i++;
+        }
+        float average = sum/n;
+        System.out.println("Average of number is : "+average);
+        return 0;
     }
 
     public static void main(String[] args) {
