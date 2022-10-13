1f32560db1fb166125b56d787b8360a769b2942f
bridgetisvirtuallyhere <bridgetjacosta@gmail.com>
2022-10-13 10:10:47 -0700

    Day 2

--- src/ForLoops.java
+++ src/ForLoops.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
-
+/*
+*@author Bridget Acosta
+* 10/13/2022
+ */
 public class ForLoops {
     private final static Scanner s = new Scanner(System.in);
 
@@ -16,29 +19,44 @@ public class ForLoops {
     }
 
 
-//In the ForLoops class, create a method named rangeSum
+    //In the ForLoops class, create a method named rangeSum
     public static int rangeSum() {
         //that sums a range/interval of numbers,
-
+        System.out.println("Number? ");
+        int firstInput = Integer.parseInt(s.nextLine());
+        System.out.println("Number? ");
+        int secondInput = Integer.parseInt(s.nextLine());
+        int range = 0;
+        if (firstInput < secondInput) {
+            for (int i = firstInput; i <= secondInput; i++) {
+                range += i;
+                System.out.println("The sum of the range is: " + range);
+            }
+        } else {
+            for (int i = secondInput; i <= firstInput; i++) {
+                range += i;
+                System.out.println("The sum of the range is: " + range);
+            }
+            //and prints the range.
+            //Your program should not care if the smaller or larger numbers are entered first:
+        }
+        return firstInput;
     }
-    public static void factorial() {
-    }
 
-    public static void main(String[] args) {
+        public static void factorial () {//In the ForLoops class, implement a method named factorial
+        //that calculates the factorial of a number read in from System.in
+            System.out.println("Number? ");
+            int input = Integer.parseInt(s.nextLine());
+            int factorial = 1;
+            for (int i = 2; i <= input; i++) {
+                factorial = factorial * i;
+            }
+            System.out.println("The factorial is: " + factorial);
+        }
 
-        rangeSum();
-        factorial();
+        public static void main (String[]args){
+            forSum();
+            rangeSum();
+            factorial();
+        }
     }
-}
-// System.out.println("Number? ");
-//        int firstInput = Integer.parseInt(s.nextLine());
-//        System.out.println("Number? ");
-//        int secondInput = Integer.parseInt(s.nextLine());
-//        int range = 0;
-//        for (int i = firstInput; i <= secondInput; i++) {
-//            range += i;
-//        }
-//        System.out.println("The sum of the range is: " + range);
-//        return range;
-//        //and prints the range.
-//        //Your program should not care if the smaller or larger numbers are entered first:
--- src/ReWrites.java
+++ src/ReWrites.java
@@ -1,40 +1,48 @@
 import java.util.Scanner;
-
+/*
+@author Bridget Acosta
+10/13/2022
+ */
 class ReWrites {
     private final static Scanner s = new Scanner(System.in);
 
-    public static void avgRewrite() {
+    public static void avgRewrite() { //In a class named ReWrites, rewrite the while loops in the avgRewrite method
+        // as a for loop instead of a while loop.
+
         String l;
         double sum = 0.0;
         int count = 0;
         System.out.println("Ths program will calculate the average of numbers entered. Begin entering and enter \"Q\" to quit.");
-        while(!(l = s.nextLine()).toUpperCase().equals("Q")) {
+        //while(!(l = s.nextLine()).toUpperCase().equals("Q"))
+        for (l = s.nextLine(); !l.toUpperCase().equals("Q"); l = s.nextLine()){
             try {
                 sum += Integer.parseInt(l);
                 count++;
-            } catch (NumberFormatException nfe) {}
+            } catch (NumberFormatException nfe) {
+                throw new RuntimeException(nfe);
+            }
         }
 
         System.out.printf("Average is: %f%n", (sum/count));
     }
-    public static void sumRewrite() {
+    public static void sumRewrite() { //In a class named ReWrites, rewrite the while loops in the sumRewrite method
+        // without the break condition and the boolean true in the parenthesis.
         Scanner in = new Scanner(System.in);
         System.out.println("Type positive integers to sum. To stop, type 0...");
-        int x = -1;
+        int x = 1;
         int sum = 0;
 
-        while (true) {
+        while (x > 0) {
             try {
-                if(x == 0) {
-                    break;
-                }
                 x = Integer.parseInt(in.nextLine());
                 if (x <= 0) {
                     continue;
                 }
                 System.out.println("Adding " + x);
                 sum += x;
-            } catch (NumberFormatException nfe) {}
+            } catch (NumberFormatException nfe) {
+                throw new RuntimeException(nfe);
+            }
         }
 
         System.out.printf("Sum: %d%nGood Bye%n", sum);
